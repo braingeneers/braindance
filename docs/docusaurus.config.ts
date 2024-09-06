bb363d585fe1a92292fe692bccd59a43ea501c8c
Ash <ashsrobbins@gmail.com>
2024-09-06 11:03:30 -0700

    Changing doc link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,64 +1,62 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'BrainDance',
-  tagline: 'Making neural stimulation easy',
-  favicon: 'img/favicon.ico',
+  title: "BrainDance",
+  tagline: "Making neural stimulation easy",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://braingeneers.github.io',
+  url: "https://braingeneers.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/braindance/',
+  baseUrl: "/braindance/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'braingeneers', // Usually your GitHub org/user name.
-  projectName: 'braindance', // Usually your repo name.
-  deploymentBranch: 'docs',
+  organizationName: "braingeneers", // Usually your GitHub org/user name.
+  projectName: "braindance", // Usually your repo name.
+  deploymentBranch: "docs",
   trailingSlash: false, // Remove trailing slashes from URLs
 
-  onBrokenLinks: 'warn',//'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn", //'throw',
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/braingeneers/braindance/',
+          editUrl: "https://github.com/braingeneers/braindance/tree/docs-edit/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/braingeneers/braindance/',
+          editUrl: "https://github.com/braingeneers/braindance/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -66,38 +64,37 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'BrainDance',
+      title: "BrainDance",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/braingeneers/braindance',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/braingeneers/braindance",
+          label: "GitHub",
+          position: "right",
         },
-        
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Docs',
-              to: '/docs/',
+              label: "Docs",
+              to: "/docs/",
             },
           ],
         },
@@ -119,15 +116,15 @@ const config: Config = {
           // ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             // {
             //   label: 'Blog',
             //   to: '/blog',
             // },
             {
-              label: 'GitHub',
-              href: 'https://github.com/braingeneers/braindance',
+              label: "GitHub",
+              href: "https://github.com/braingeneers/braindance",
             },
           ],
         },
@@ -137,11 +134,11 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['python', 'bash', 'json'], 
+      additionalLanguages: ["python", "bash", "json"],
     },
     // Add syntax highlighting for inline code
     colorMode: {
-      defaultMode: 'light',
+      defaultMode: "light",
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
